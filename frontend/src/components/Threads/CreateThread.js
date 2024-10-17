e529e850fc7383c2f33da6b6d507452d1416e084
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-10-17 21:13:44 +0100

    updated for creating specifik threads with forumType

--- frontend/src/components/Threads/CreateThread.js
+++ frontend/src/components/Threads/CreateThread.js
@@ -1,20 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import UseFetchProfile from "../../hooks/FetchProfile";
 
-const CreateThread = () => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
+const CreateThread = ({ forumType }) => {
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+    author: "",
+    forumType: forumType,
+  });
+
+  const { user, loading, error } = UseFetchProfile();
+
+  useEffect(() => {
+    if (user) {
+      setFormData((prevData) => ({
+        ...prevData,
+        author: user.username,
+      }));
+    }
+  }, [user]);
+
+  if (loading) {
+    return <p>Loading profile...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/thread", {
-        title,
-        content,
-        author,
+      await axios.post("http://localhost:5000/thread", formData);
+      alert("Thread created successfully in the " + forumType + " forum!");
+      //reset without windows.relode
+      setFormData({
+        title: "",
+        content: "",
+        author: user.username,
+        forumType: forumType,
       });
-      alert("Thread created successfully!");
     } catch (error) {
       console.error("Error creating thread:", error);
     }
@@ -22,41 +52,42 @@ const CreateThread = () => {
 
   return (
     <>
-      <div className="grid justify-center">
-        <form onSubmit={handleSubmit}>
-          <div className="">
-            <label>Title on your thread:</label>
-            <input
-              className="text-black"
-              type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label>Content:</label>
-            <textarea
-              className="text-black"
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label>Author:</label>
-            <input
-              className="text-black"
-              type="text"
-              value={author}
-              onChange={(e) => setAuthor(e.target.value)}
-              required
-            />
-          </div>
-          <div className="bg-black size-fit p-1 rounded-lg flex ">
-            <button type="submit">Create Thread</button>
-          </div>
-        </form>
+      <div className="bg-cyan-950 text-white w-2/4 mx-auto text-center mt-8 rounded-lg">
+        <h1 className="text-3xl font-bold pt-5 mb-6">
+          Create Thread in {forumType} Forum
+        </h1>
+        <ul className="space-y-4 text-black flex flex-col items-center pb-5">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <input
+                className="text-black"
+                placeholder="Title"
+                type="text"
+                name="title"
+                value={formData.title}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <textarea
+                className="text-black mb-4 w-full h-20"
+                placeholder="Content"
+                type="content"
+                name="content"
+                value={formData.content}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <p className="text-white">Author: {user.username}</p>
+            </div>
+            <div className="flex bg-gray-500 p-2 rounded-lg mt-3 text-white">
+              <button type="submit">Create Thread</button>
+            </div>
+          </form>
+        </ul>
       </div>
     </>
   );
