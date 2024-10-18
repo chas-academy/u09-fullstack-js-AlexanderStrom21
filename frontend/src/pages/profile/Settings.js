32c098e316e85f759e18c6928efb5519f38f10f5
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-10-18 17:38:36 +0100

    Settings created, routing for editing user info

---
+++ frontend/src/pages/profile/Settings.js
@@ -0,0 +1,10 @@
+import ProfileNameChange from "../../components/profileSettings/NameChange";
+
+const UserSettings = () => {
+  return (
+    <>
+      <ProfileNameChange />
+    </>
+  );
+};
+export default UserSettings;
