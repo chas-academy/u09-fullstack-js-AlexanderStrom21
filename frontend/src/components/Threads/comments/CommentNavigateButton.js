eaf1255a2a9733ef4bb9e7b5464dd68395af11a4
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-10-20 18:47:04 +0100

    Navigate to comment button

---
+++ frontend/src/components/Threads/comments/CommentNavigateButton.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const GetThreadIdButton = ({ threadId }) => {
+  const [thread, setThread] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchThread = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/thread/${threadId}`
+        );
+        setThread(response.data);
+        setLoading(false);
+      } catch (err) {
+        setError("Error fetching thread data");
+        setLoading(false);
+      }
+    };
+
+    if (threadId) {
+      fetchThread();
+    }
+  }, [threadId]);
+
+  const handleCommentPage = async (e) => {
+    e.preventDefault();
+    try {
+      if (thread) {
+        navigate(`/commentPage/${threadId}`);
+      } else {
+        alert("Error: No thread found!");
+      }
+    } catch (error) {
+      console.error("Error: Could not find thread", error);
+    }
+  };
+
+  if (loading) {
+    return <p>Loading thread...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  return (
+    <button
+      type="button"
+      className="bg-blue-500 text-white rounded-lg px-3 py-1"
+      onClick={handleCommentPage}
+    >
+      Comment
+    </button>
+  );
+};
+
+export default GetThreadIdButton;
