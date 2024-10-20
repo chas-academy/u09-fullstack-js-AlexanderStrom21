1f39ae4d76acb4ca184a71f5511e0cbde187390c
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-10-20 20:50:48 +0100

    routing for Users comments

--- frontend/src/pages/profile/DashBoard.js
+++ frontend/src/pages/profile/DashBoard.js
@@ -1,9 +1,11 @@
+import CommentsCreatedByUser from "../../components/Threads/comments/CommentsCreatedByUser";
 import ThreadsCreatedByUser from "../../components/Threads/ThreadsCreatedByUser";
 
 const DashBoard = () => {
   return (
     <>
       <ThreadsCreatedByUser />
+      <CommentsCreatedByUser />
     </>
   );
 };
