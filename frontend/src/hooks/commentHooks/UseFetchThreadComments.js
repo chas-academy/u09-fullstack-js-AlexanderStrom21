0679f554267ac5b8b6d6a8657a6e70a2ee7ee5a6
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-10-21 16:47:37 +0100

    FetchesCommets as a hook

---
+++ frontend/src/hooks/commentHooks/UseFetchThreadComments.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const UseFetchThreadComments = (threadId) => {
+  const [comments, setComments] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchThreadComments = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/thread/${threadId}/comments`
+        );
+        setComments(response.data);
+        setLoading(false);
+      } catch (err) {
+        setError("Error fetching comments data");
+        setLoading(false);
+      }
+    };
+
+    if (threadId) {
+      fetchThreadComments();
+    }
+  }, [threadId]);
+
+  return { comments, loading, error };
+};
+
+export default UseFetchThreadComments;
