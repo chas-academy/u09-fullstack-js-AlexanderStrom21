96de1da0d36c3d1d1ff7f267090c6b70e577cb6a
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-10-21 17:50:13 +0100

    made changes so the fetch by authorId dosent effect. Admin

--- frontend/src/hooks/commentHooks/UseFetchCommentsByAuthorId.js
+++ frontend/src/hooks/commentHooks/UseFetchCommentsByAuthorId.js
@@ -11,16 +11,27 @@ const useFetchCommentsByAuthorId = () => {
   const username = user?.username;
 
   useEffect(() => {
-    if (threads && threads.length > 0 && username) {
-      const filteredComments = threads.flatMap((thread) =>
-        thread.comments
-          .filter((comment) => comment.author === username)
-          .map((comment) => ({ ...comment, threadId: thread._id }))
-      );
-      setUserThreadComments(filteredComments);
-      setUserCommentsAndAuthorMatch(filteredComments.length > 0);
+    if (threads && threads.length > 0) {
+      if (user?.isAdmin) {
+        const allComments = threads.flatMap((thread) =>
+          thread.comments.map((comment) => ({
+            ...comment,
+            threadId: thread._id,
+          }))
+        );
+        setUserThreadComments(allComments);
+        setUserCommentsAndAuthorMatch(allComments.length > 0);
+      } else {
+        const filteredComments = threads.flatMap((thread) =>
+          thread.comments
+            .filter((comment) => comment.author === username)
+            .map((comment) => ({ ...comment, threadId: thread._id }))
+        );
+        setUserThreadComments(filteredComments);
+        setUserCommentsAndAuthorMatch(filteredComments.length > 0);
+      }
     }
-  }, [threads, username]);
+  }, [threads, username, user]);
 
   return { userCommentsAndAuthorMatch, userThreadComments, loading, error };
 };
