a4fdc40f59653148a81c8d8f468a05d235067e3e
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-10-22 16:59:11 +0100

    changed frontend url for netlify

--- backend/index.js
+++ backend/index.js
@@ -5,24 +5,19 @@ const cors = require("cors");
 const usersRoute = require("./routes/UsersRoute"); // Import your users route
 const threadRoutes = require("./routes/Threads");
 
-// Initialize dotenv to read .env file
 dotenv.config();
 
-// Create Express app
 const app = express();
 
-// Middleware to parse JSON
 app.use(express.json());
 
-// CORS configuration
 app.use(
   cors({
-    origin: "https://your-netlify-site.netlify.app", // Allow your frontend URL
+    origin: "https://purposecoder.netlify.app", // Allow your frontend URL
     credentials: true,
   })
 );
 
-// MongoDB connection using mongoose
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -31,14 +26,11 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
-// Register the routes under the base URL
 app.use("/", threadRoutes);
 app.use("/", usersRoute);
 
-// Get PORT from environment variables or default to 5000
 const PORT = process.env.PORT || 5000;
 
-// Start the Express server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
