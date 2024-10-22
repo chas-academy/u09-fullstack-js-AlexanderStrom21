63481fe302d23f35728965b37e016080a4c9ba5f
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-10-22 19:22:40 +0100

    removed useNewUrlParser: true line cuz render complain

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,7 @@ app.use(
 );
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-  })
+  .connect(process.env.MONGO_URI, {})
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
