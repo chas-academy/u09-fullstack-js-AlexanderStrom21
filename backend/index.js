4cad08f0a47a612fa2a6e89f6e47486ad07f905e
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-10-22 19:23:29 +0100

    took it back everything crashed

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,10 @@ app.use(
 );
 
 mongoose
-  .connect(process.env.MONGO_URI, {})
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
