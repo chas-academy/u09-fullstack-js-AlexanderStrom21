429a83d0f2b2d713ca64720b74785f50c928ce35
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-10-22 21:08:22 +0100

    login cookie try

--- backend/routes/UsersRoute.js
+++ backend/routes/UsersRoute.js
@@ -73,10 +73,7 @@ router.post("/login", async (req, res) => {
     const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
       expiresIn: "1h",
     });
-
-    res
-      .cookie("token", token, { httpOnly: true })
-      .json({ message: "Login successful" });
+    res.status(200).json({ token });
   } catch (err) {
     res.status(500).json({ message: "Server error" });
   }
@@ -111,10 +108,14 @@ const authMiddleware = (req, res, next) => {
 };
 
 // Fetch user profile
-router.get("/profile", authMiddleware, async (req, res) => {
+router.get("/profile", async (req, res) => {
   try {
-    const user = await User.findById(req.userId).select("-password"); // Get user info without password
-    res.json(user);
+    const decoded = jwt.verify(token, JWT_SECRET);
+    const user = await User.findById(decoded.userId).select("-password"); // Get user info without password
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
   } catch (err) {
     console.error("Error fetching user profile:", err);
     res.status(500).json({ message: "Server error" });
