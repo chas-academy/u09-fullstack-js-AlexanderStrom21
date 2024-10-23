7076db0d5b956d64cd114e9d02dc60d92cca1d2c
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-10-23 14:49:09 +0100

    styling

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"], // Set Poppins as the standard font
+      },
+      backgroundImage: {
+        "main-gradient": "linear-gradient(to top, #c471f5 0%, #fa71cd 100%)", //bg-main-gradient
+      },
+      colors: {
+        primary: "#c471f5", // Use for buttons, links, etc.
+        secondary: "#fa71cd", // Use for accents
+        text: "#ffffff", // Main text color
+        background: "rgba(255, 255, 255, 0.9)",
+      },
+    },
   },
   plugins: [],
 };
