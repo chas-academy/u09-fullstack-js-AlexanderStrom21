20ba4a3220ee9eb77023eeefb8192b6f0c45b36c
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-10-23 11:14:07 +0100

    Import typeo

--- backend/routes/ThreadRoute.js
+++ backend/routes/ThreadRoute.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const threadController = require("../controllers/ThreadController");
+const threadController = require("../contollers/ThreadController");
 const authMiddleware = require("../Middleware/AuthMiddleware");
-
 const router = express.Router();
 
 // Get all threads
