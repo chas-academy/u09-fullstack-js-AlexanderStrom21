5ad65e4142bf9d4e6b590fc627ce99eafbffe645
alexander <144438660+AlexanderStrom21@users.noreply.github.com>
2024-10-24 03:00:10 +0100

    tailwind comments, styling in app.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,20 +7,20 @@ module.exports = {
         sans: ["Poppins", "sans-serif"], // Set Poppins as the standard font
       },
       backgroundImage: {
-        "main-gradient": "linear-gradient(to top, #9b59b6 0%, #d94d92 100%)", //bg-main-gradient
+        "main-gradient": "linear-gradient(to top, #9b59b6 0%, #d94d92 100%)", //bg-main-gradient Root-bg
       },
       colors: {
-        background: "#2E073F", //Cards
-        primary: "#7A1CAC", // Use for buttons, links, etc.
-        secondary: "#AD49E1", // Use for accents
-        text: "#EBD3F8", // Main text color
-        navbar: "#2E073F", // Dark background for navbar
-        dark: "#2d3748", // Dark text color for better readability on light backgrounds
-        warning: "#dd6b20", // Warning color (orange)
-        error: "#e53e3e", // Error color (red)
-        info: "#f8589b", // Info color (pink)
-        primaryhover: "#a054e7", // Darker shade for primary button hover
-        infohover: "#a35a7e", //pinkhover
+        background: "#2E073F", //main Cards
+        primary: "#7A1CAC", //secound color thats above main card
+        secondary: "#AD49E1", // third color thats above primary or to mix with for more contrast
+        text: "#EBD3F8", // Main that isnt 100% white text color
+        navbar: "#2E073F", // background for navbar
+        dark: "#2d3748", // Dark text for white forms
+        warning: "#dd6b20", // delete button color (orange)
+        error: "#e53e3e", // delete hover color (red)
+        info: "#f8589b", // buttons that isnt a delete color (pink)
+        primaryhover: "#a054e7", // purple hover
+        infohover: "#a35a7e", //pink hover
       },
     },
   },
